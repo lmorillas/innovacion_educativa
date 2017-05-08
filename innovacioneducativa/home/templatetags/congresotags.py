094972cca62e70044642786df2b423431fa234f6
lmorillas <morillas@gmail.com>
2017-05-08 12:43:24 +0200

    número de inscritos

--- innovacioneducativa/home/templatetags/congresotags.py
+++ innovacioneducativa/home/templatetags/congresotags.py
@@ -2,9 +2,13 @@ from django import template
 from asistentes.models import UsuarioTalleres
 
 
+
 register = template.Library()
 
 
 @register.simple_tag
 def participantes():
+	'''
+	Devuelve número de asistentes que han confirmado ya el email y se han inscrito
+	'''
     return UsuarioTalleres.objects.count()
